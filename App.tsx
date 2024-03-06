d6e767768842a49b7d7f27015297078b9c2abd0c
Jixatos <j.gody2004@gmail.com>
2024-03-06 11:58:52 -0300

    Componentização do Header

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
