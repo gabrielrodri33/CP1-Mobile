a23bbd139ab5e1c89fc0d7f778cd8e5bb3efa656
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-03-07 19:00:21 -0300

    Criado e iniciado estilização de um item de compra

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
+import { StyleSheet, ScrollView, View, SafeAreaView } from "react-native";
+import Home from "./components/Home";
 
 export default function App() {
   return (
-    <Home />
+    <ScrollView>
+      <View>
+        <SafeAreaView>
+          <Home />
+        </SafeAreaView>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,15 +1,74 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import React from "react";
 
 export default function Home() {
   return (
-    <View id='2'>
-        <Image
-            source={require('../img/produto (1).png')}
-        ></Image>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cumque maiores, fugit accusamus illum pariatur nemo reiciendis sapiente! Laboriosam nisi asperiores enim atque repudiandae debitis iste minima veniam a deleniti.</Text>
-      <Text>R$2.000,00</Text>
+    <View style={style.container}>
+      <View style={style.container_item}>
+        <View>
+          <Image
+            source={require("../img/produto (1).png")}
+            style={style.tam_image}
+          ></Image>
+          <Text style={style.price}>R$2.000,00</Text>
+        </View>
+        <View style={style.text_container}>
+          <Text style={style.title_item}>Playstation 4 PRO</Text>
+          <Text style={style.desc}>
+            Explore o poder do gaming com o PS4 Pro novo! Gráficos
+            impressionantes e desempenho excepcional. Não perca, adquira já o
+            seu!
+          </Text>
+          <Image
+            source={require("../img/five_stars.png")}
+            style={style.tam_stars}
+          ></Image>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
+
+const { width } = Dimensions.get("window");
+
+const style = StyleSheet.create({
+  title_item: {
+    fontSize: 20,
+  },
+  text_container: {
+    flex: 1,
+    marginLeft: 6,
+  },
+  price: {
+    fontSize: 23,
+  },
+  desc: {
+    marginRight: 3,
+  },
+  container_item: {
+    width: width * 0.93,
+    height: width * 0.45,
+    marginVertical: 10,
+    borderRadius: 6,
+    alignItems: 'center',
+    padding: 3,
+    flexDirection: "row",
+    backgroundColor: "#fafaaa",
+  },
+  tam_image: {
+    marginTop: 5,
+    width: width * 0.25,
+    height: width * 0.25,
+  },
+  tam_stars: {
+    marginVertical: 10,
+    // width: 'auto',
+    aspectRatio: 5,
+    height: width * 0.04,
+  },
+  container: {
+    marginTop: 5,
+    marginHorizontal: width * 0.01,
+    paddingHorizontal: 8,
+  },
+});
