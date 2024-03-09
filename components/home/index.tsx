79373ad0388abee4626fe11575fc18bbc0d56bbe
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-03-09 12:45:20 -0300

    Dimensions

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import Items from "./Items";
 
