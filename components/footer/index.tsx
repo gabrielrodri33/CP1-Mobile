e35155e6e9a587afa467d0dfd5b03eade5a492c6
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-03-09 12:45:20 -0300

    Dimensions

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import React from "react";
 
 export default function Footer() {
@@ -52,6 +52,8 @@ export default function Footer() {
     </View>
   );
 }
+
+const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   footer: {
