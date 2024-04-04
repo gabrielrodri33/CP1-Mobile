7a20b6edb57734f327b38aac6ad880883c0a28f0
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-04-04 19:45:12 -0300

    Consertado bug de import

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -2,9 +2,9 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 // import { Entypo } from '@expo/vector-icons';
 
-import Produtos from '../screens/Produtos/index';
+import produtos from '../screens/produtos/index';
 import Devs from '../screens/Devs/index';
-import LogIn from "../screens/Login/index";
+import login from "../screens/login/index";
 import LogOut from "../screens/Logout/index";
 import SigIn from "../screens/Sigin";
 
@@ -17,14 +17,14 @@ export default function DrawerRoutes() {
     <Navigator>
       <Screen
         name = "Produtos"
-        component={Produtos}
+        component={produtos}
         options={{ 
           drawerIcon: ()=> <Entypo name="home" size={24} color="black" />
         }}
       />
       <Screen 
         name = "LogIn"
-        component={LogIn}
+        component={login}
         options={{ 
           headerTitle: "",
           drawerIcon: ()=> <Entypo name="login" size={24} color="black" />
