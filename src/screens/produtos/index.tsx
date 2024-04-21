059b5846aac964174f7442138ce1cff212dd3079
jixatos <j.gody2004@gmail.com>
2024-04-21 07:07:17 -0300

    Conclusão da Página Produtos

--- src/screens/produtos/index.tsx
+++ src/screens/produtos/index.tsx
@@ -1,29 +1,46 @@
 import React, { useContext } from "react";
-import { ScrollView, useColorScheme, View, FlatList } from "react-native";
+import { useColorScheme, FlatList, StyleSheet, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Avatar, Button, Icon, ListItem, Text } from "@rneui/base";
-import { Rating } from '@rneui/themed';
+import { Avatar, ListItem } from "@rneui/base";
 
 import ItensContext from "../../context/ItensContext";
 
 export default function Produtos() {
   const colorScheme = useColorScheme();
 
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+
   const theme = colorScheme === "dark" ? "#363636" : "#fff";
+  const textColor = colorScheme === "dark" ? "#fff" : "#000";
+  // style={{ color: textColor }} Estilização para o texto dependendo do theme
 
   const { estado } = useContext(ItensContext);
 
   function getItens({ item }) {
     return (
-      <ListItem>
-        <Avatar source={{uri: item.image}}/>
+      <ListItem
+        containerStyle={{
+          backgroundColor: theme,
+          paddingVertical: height * 0.02,
+          paddingHorizontal: width * 0.03,
+          borderBottomWidth: 1,
+          borderBottomColor: textColor,
+        }}
+      >
+        <Avatar source={{ uri: item.image }} size={80} />
         <ListItem.Content>
-          <ListItem.Title>{item.title}</ListItem.Title>
-          <ListItem.Subtitle>{item.price}</ListItem.Subtitle>
-          
+          <ListItem.Title style={{ color: textColor }}>
+            {item.title}
+          </ListItem.Title>
+          <ListItem.Subtitle style={{ color: textColor }}>
+            {item.price}
+          </ListItem.Subtitle>
         </ListItem.Content>
         <ListItem.Content>
-          <ListItem.Subtitle>{item.desc}</ListItem.Subtitle>
+          <ListItem.Subtitle style={{ color: textColor }}>
+            {item.desc}
+          </ListItem.Subtitle>
         </ListItem.Content>
       </ListItem>
     );
@@ -31,7 +48,11 @@ export default function Produtos() {
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme }}>
-      <FlatList data={estado.DadosItens} renderItem={getItens} />
+      <FlatList
+        data={estado.DadosItens}
+        renderItem={getItens}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </SafeAreaView>
   );
 }
